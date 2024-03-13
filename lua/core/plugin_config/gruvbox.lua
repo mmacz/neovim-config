81c667956ba077f8c66b7d47b8315f9480004e3a
Marek Macznik <marek.macznik@dolby.com>
2024-03-13 17:01:24 +0100

    Set transparent bg for gruvbox

--- lua/core/plugin_config/gruvbox.lua
+++ lua/core/plugin_config/gruvbox.lua
@@ -1,2 +1,5 @@
 vim.opt.termguicolors = true
+vim.g.gruvbox_transparent_bg = true
+vim.g.gruvbox_contrast_dark = 'soft'
 vim.cmd [[ colorscheme gruvbox ]]
+vim.cmd [[ hi Normal guibg=NONE ctermbg=NONE ]]
