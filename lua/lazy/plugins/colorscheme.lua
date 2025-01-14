0da5f5a0bdbccb6c2733f1c9b23784e435a778f2
Marek Macznik <marek.macznik@gmail.com>
2025-01-15 00:47:05 +0100

    Switch colorscheme to catppuccin

--- lua/lazy/plugins/colorscheme.lua
+++ lua/lazy/plugins/colorscheme.lua
@@ -1,10 +1,10 @@
 return {
-	"bluz71/vim-nightfly-colors",
-  "ellisonleao/gruvbox.nvim",
-  "folke/tokyonight.nvim",
-	priority = 1000,
+	-- "bluz71/vim-nightfly-colors",
+	--  "ellisonleao/gruvbox.nvim",
+	--  "folke/tokyonight.nvim",
+  "catppuccin/nvim", name = "catppuccin", priority = 1000,
 	config = function()
-		vim.cmd([[colorscheme tokyonight-night]])
+		vim.cmd([[colorscheme catppuccin]])
 	end,
 }
 
