dc92980ffaacb0f49593187b9789c263d7590618
Marek Macznik <marek.macznik@gmail.com>
2025-01-15 00:32:38 +0100

    Add: tokyonight colorscheme

--- lua/mmacz/plugins/colorscheme.lua
+++ lua/mmacz/plugins/colorscheme.lua
@@ -1,8 +1,10 @@
 return {
 	"bluz71/vim-nightfly-colors",
+  "ellisonleao/gruvbox.nvim",
+  "folke/tokyonight.nvim",
 	priority = 1000,
 	config = function()
-		vim.cmd([[colorscheme nightfly]])
+		vim.cmd([[colorscheme tokyonight-night]])
 	end,
 }
 
