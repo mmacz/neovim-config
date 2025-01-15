3b4fb25be4e5dfa8b9242fc997d6689e44cf1935
Marek Macznik <marek.macznik@gmail.com>
2025-01-15 11:54:56 +0100

    Oldworld colorscheme

--- lua/lazy/plugins/colorscheme.lua
+++ lua/lazy/plugins/colorscheme.lua
@@ -1,10 +1,11 @@
 return {
-	-- "bluz71/vim-nightfly-colors",
-	--  "ellisonleao/gruvbox.nvim",
-	--  "folke/tokyonight.nvim",
-  "catppuccin/nvim", name = "catppuccin", priority = 1000,
+	-- "bluz71/vim-nightfly-colors", lazy = false, priority = 1000,
+	--  "ellisonleao/gruvbox.nvim", lazy = false, priority = 1000,
+	--  "folke/tokyonight.nvim", lazy = false, priority = 1000,
+  -- "catppuccin/nvim", name = "catppuccin", priority = 1000,
+  "dgox16/oldworld.nvim", lazy = false, priority = 1000,
 	config = function()
-		vim.cmd([[colorscheme catppuccin]])
+		vim.cmd([[colorscheme oldworld]])
 	end,
 }
 
