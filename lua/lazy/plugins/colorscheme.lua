84a7680d91c325dedb88d6e44d0f9ab5011438d2
Marek Macznik <marek.macznik@gmail.com>
2025-01-21 23:08:29 +0100

    Change theme to tokyonight

--- lua/lazy/plugins/colorscheme.lua
+++ lua/lazy/plugins/colorscheme.lua
@@ -1,12 +1,12 @@
 return {
 	-- "bluz71/vim-nightfly-colors", lazy = false, priority = 1000,
 	--  "ellisonleao/gruvbox.nvim", lazy = false, priority = 1000,
-	--  "folke/tokyonight.nvim", lazy = false, priority = 1000,
+	"folke/tokyonight.nvim", lazy = false, priority = 1000,
   -- "catppuccin/nvim", name = "catppuccin", priority = 1000,
   -- "dgox16/oldworld.nvim", lazy = false, priority = 1000,
-  "shaunsingh/nord.nvim", lazy = false, priority = 1000,
+  -- "shaunsingh/nord.nvim", lazy = false, priority = 1000,
 	config = function()
-		vim.cmd([[colorscheme nord]])
+		vim.cmd([[colorscheme tokyonight-storm]])
 	end,
 }
 
