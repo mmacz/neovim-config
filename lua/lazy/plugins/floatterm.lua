274aab1465c91c2ffe8bb315f3a5d68f38db495b
Marek Macznik <marek.macznik@dolby.com>
2025-01-29 15:46:59 +0100

    Remove unusable floatterm keybindings

--- lua/lazy/plugins/floatterm.lua
+++ lua/lazy/plugins/floatterm.lua
@@ -2,7 +2,5 @@ return {
   "voldikss/vim-floaterm",
   config = function()
     vim.keymap.set('n', '<C-n>', ":FloatermNew<CR>", {})
-    vim.keymap.set('t', '<C-t>', ":FloatermToggle<CR>", {})
-    vim.keymap.set('t', '<C-k>', ":FloatermKill!<CR>", {})
   end
 }
