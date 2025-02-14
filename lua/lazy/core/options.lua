d91843408940aab6c9cc99d1abb82e0e5e914acd
Marek Macznik <marek.macznik@gmail.com>
2025-02-14 23:46:14 +0100

    Remove trailing whitespaces

--- lua/lazy/core/options.lua
+++ lua/lazy/core/options.lua
@@ -1,25 +1,24 @@
 vim.g.netrw_banner = 0
 vim.g.netrw_altv = 1
 vim.g.netrw_liststyle = 3
-vim.opt.relativenumber = true 
-vim.opt.number = true 
-vim.opt.tabstop = 2 
-vim.opt.shiftwidth = 2 
-vim.opt.expandtab = true 
-vim.opt.autoindent = true 
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
 vim.opt.title = true
-vim.opt.wrap = false 
-vim.opt.ignorecase = true 
-vim.opt.smartcase = true 
-vim.opt.cursorline = true 
+vim.opt.wrap = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.cursorline = true
 vim.opt.termguicolors = true
-vim.opt.background = "dark" 
-vim.opt.signcolumn = "yes" 
+vim.opt.signcolumn = "yes"
 vim.opt.scrolloff = 15
 vim.opt.syntax = "on"
-vim.opt.backspace = "indent,eol,start" 
-vim.opt.clipboard:append("unnamedplus") 
-vim.opt.splitright = true 
-vim.opt.splitbelow = true 
+vim.opt.backspace = "indent,eol,start"
+vim.opt.clipboard:append("unnamedplus")
+vim.opt.splitright = true
+vim.opt.splitbelow = true
 vim.opt.swapfile = false
 
