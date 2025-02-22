da6df15de7a80e0e608ea2fad946fa18f4ceefcb
Marek Macznik <marek.macznik@gmail.com>
2025-02-23 00:14:17 +0100

    Set up DAP + autocmd for loading .vscode/launch.json

--- lua/lazy/core/autocmds.lua
+++ lua/lazy/core/autocmds.lua
@@ -10,3 +10,11 @@ vim.api.nvim_create_autocmd('BufWritePre', {
     command = '%s/\\s\\+$//e',
 })
 
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = ".vscode/launch.json",
+  callback = function()
+    require('dap.ext.vscode').load_launchjs(nil, { lldb = { "c", "cpp", "rust" } })
+    print("Reloaded launch.json file.")
+  end
+})
+
---
+++ lua/lazy/plugins/dap.lua
@@ -0,0 +1,48 @@
+return {
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "racrriga/nvim-dap-ui",
+    "nvim-neotest/nvim-nio"
+  },
+  config = function()
+    local dap = require("dap")
+    local dapui = require("dapui")
+    local vscode = require('dap.ext.vscode')
+
+    dapui.setup()
+
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+
+    dap.adapters.lldb = {
+      type = 'executable',
+      command = 'lldb-dap',
+      name = 'lldb'
+    }
+
+    vscode.load_launchjs(nil, {
+      lldb = { "c", "cpp", "rust"},
+      cppdbg = { "c", "cpp", "rust"},
+      }
+    )
+
+    vim.keymap.set("n", "<leader>db", function() dap.toggle_breakpoint() end, { desc = "DAP: Toggle breakpoint" })
+    vim.keymap.set("n", "<leader>dB", function() dap.toggle_breakpoint(vim.fn.input("Condition: ")) end, { desc = "DAP: Toggle conditional breakpoint" })
+    vim.keymap.set("n", "<leader>dso", function() dap.step_over() end, { desc = "DAP: Step over" })
+    vim.keymap.set("n", "<leader>dsi", function() dap.step_into() end, { desc = "DAP: Step into" })
+    vim.keymap.set("n", "<leader>dst", function() dap.step_out() end, { desc = "DAP: Step out" })
+    vim.keymap.set("n", "<leader>dc", function() dap.continue() end, { desc = "DAP: Continue" })
+    vim.keymap.set("n", "<leader>dt", function() dap.terminate() end, { desc = "DAP: Terminate " })
+  end,
+}
+
